adacfa7ce98bc4adf2193e950da3adb797e47642
Jesse Fender <jessefender@jessefender.com>
2023-10-22 16:39:49 -0500

    Added a connect function using the dot env class loading in the properties from that. also we are expecting config to just function based on the new file load, so no more default loading. added property for $RootPath from config array

--- app/Application.php
+++ app/Application.php
@@ -3,19 +3,35 @@
 	namespace Jesse\SimplifiedMVC;
 	
 	use Jesse\SimplifiedMVC\Database\Connection;
+	use Jesse\SimplifiedMVC\utilities\DotEnv;
 	
 	class Application
 	{
 		public static Application $app;
-		
+		public static string $RootPath;
 		
 		public Connection $connection;
 		
-		function __construct(array $config = ["database" => 'pdo'])
+		function __construct(array $config)
 		{
 			// do initialization stuff...
-			$this->connection = new Connection($config['database']);
+			Application::$RootPath = $config['rootPath'];
+			DotEnv::load($config['envPath']);
+			$this->connect();
 			// set the constant var $app to this instance
 			Application::$app = $this;
+		}
+		
+		function connect() : void
+		{
+			$this->connection = new Connection($_ENV['DATABASE_CONNECTOR']);
+			$this->connection->type = $_ENV['DATABASE_TYPE'];
+			$this->connection->name = $_ENV['DATABASE_NAME'];
+			$this->connection->user = $_ENV['DATABASE_USER'];
+			$this->connection->pass = $_ENV['DATABASE_PASS'];
+			$this->connection->port = $_ENV['DATABASE_PORT'];
+			$this->connection->host = $_ENV['DATABASE_HOST'];
+			
+			$this->connection->connect();
 		}
 	}
