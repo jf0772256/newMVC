a33902a64adc627791ab0450832014ca991b9aa4
Jesse Fender <jessefender@jessefender.com>
2023-10-22 16:37:03 -0500

    Added test code to make sure Application was loading correctly and that the connection is connecting

--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
 <?php
+	
+	use Jesse\SimplifiedMVC\Application;
+	
+	require_once __DIR__ . "/../vendor/autoload.php";
+	$config = require_once "../app/config/config.php";
+	// [
+	// 	"rootPath" => dirname(__DIR__, 1),
+	// 	"envPath" => dirname(__DIR__, 1) . "/.env"
+	// ];
+	
+	$app = new Application($config);
+	
+	$data = Application::$app->connection->ExecuteQuery("SELECT 'Hello World! Using Static' as text_col", [])->fetchObject()->text_col;
+	echo "does this show: {$data}";
