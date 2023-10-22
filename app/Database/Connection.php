c7584540f7d4dddde6a110456c25a01a6f1101a4
Jesse Fender <jessefender@jessefender.com>
2023-10-22 15:37:20 -0500

    Updated the Connect class, cutting out the use of mysqli library as we will want this to be as widespread as we can get.

---
+++ app/Database/Connection.php
@@ -0,0 +1,89 @@
+<?php
+	
+	namespace Jesse\SimplifiedMVC\Database;
+	
+	use AllowDynamicProperties;
+	use PDO;
+	use PDOStatement;
+	#[AllowDynamicProperties] class Connection extends DatabaseBase
+	{
+		/**
+		 * @param string $connectionType must be passed and must be either 'pdo' [default] or 'sqlite'
+		 */
+		public function __construct(string $connectionType = "pdo")
+		{
+			$this->connectionType = $connectionType;
+		}
+		
+		/**
+		 * Runs a query off of the database connection interface used, returning the statement object
+		 *
+		 * @param string      $sql    SQL statement to be prepared and ran
+		 * @param array|null  $params Parameters array to bind to the query through the statement object
+		 *
+		 * ```php
+		 *  //after creating connection instance use query() on the Connection instance
+		 *  $db->("SELECT * FROM table")->get_results()->fetch_all(MYSQLI_ASSOC);
+		 *  // pdo uses by default the associative arrays for array returns
+		 *  // sqlite interface is technically a wrapped pdo connection, it uses a different sql syntax.
+		 * ```
+		 *
+		 * All queries are parametrised, and you can pass parameters to the query method using the params array:
+		 * ```php
+		 *  $id = 1;
+		 *  $db->query("SELECT * FROM table WHERE id=?", [$id])->get_results()->fetch_array(MYSQLI_ASSOC);
+		 * ```
+		 *
+		 * The final parameter is not required and will be auto generated if not passed
+		 *
+		 * @return PDOStatement|bool Either a PDO or mysqli statement object or a boolean if there was an error
+		 * @author Jesse Fender
+		 */
+		public function ExecuteQuery (string $sql, ?array $params = []) : PDOStatement|bool
+		{
+				// run prepared query and return the statement object to caller;
+				$statement = $this->connection->prepare($sql);
+				$statement->execute($params);
+				return $statement;
+		}
+		
+		function connect () : void
+		{
+			try
+			{
+				if ($this->connectionType === "pdo")
+				{
+					// create connection using pdo
+					$this->connection = new PDO($this->buildDSN(), $this->user, $this->pass, $this->pdoOptions);
+					$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				} elseif ($this->connectionType === "sqlite")
+				{
+					// create connection using pdo specifically for sqlite
+					$this->connection = new PDO($this->buildDSN(), null, null, $this->pdoOptions);
+					$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				} else
+				{
+					throw new DatabaseInterfaceException('Interface not implemented', 1042);
+				}
+			} catch (\Throwable $th)
+			{
+				Utility::dieAndDumpPretty($th);
+			}
+		}
+		
+		/**
+		 * Creates the DSN st ring that is used to connect
+		 * @return string DSN connection string to use when generating a new connection.
+		 * @author Jesse Fender
+		 */
+		private function buildDSN (): string
+		{
+			if ($this->type === "sqlite")
+			{
+				return "{$this->type}:{$this->name}";
+			} else
+			{
+				return "{$this->type}:host={$this->host};port={$this->port};dbname={$this->name}";
+			}
+		}
+	}
