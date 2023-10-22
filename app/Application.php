208a7d7314a881868290fe56482ef4d1e5ab2743
Jesse Fender <jessefender@jessefender.com>
2023-10-22 15:38:31 -0500

    added a config array to the App constructor, this could be populated from index in public or some other means...like .env

--- app/Application.php
+++ app/Application.php
@@ -2,12 +2,19 @@
 	
 	namespace Jesse\SimplifiedMVC;
 	
+	use Jesse\SimplifiedMVC\Database\Connection;
+	
 	class Application
 	{
 		public static Application $app;
-		function __construct()
+		
+		
+		public Connection $connection;
+		
+		function __construct(array $config = ["database" => 'pdo'])
 		{
 			// do initialization stuff...
+			$this->connection = new Connection($config['database']);
 			// set the constant var $app to this instance
 			Application::$app = $this;
 		}
