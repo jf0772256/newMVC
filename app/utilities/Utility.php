f54dfa21065eb917a3c9423e4628acd2914e90d1
Jesse Fender <jessefender@jessefender.com>
2023-10-22 16:34:39 -0500

    Added Utility class from mvc_Framework just to reduce the number of lines I have to mess with

---
+++ app/utilities/Utility.php
@@ -0,0 +1,36 @@
+<?php
+	
+	namespace Jesse\SimplifiedMVC\utilities;
+	
+	class Utility
+	{
+		static function dieAndDump($value) {
+			echo '<pre>';
+			var_dump($value);
+			echo '</pre>';
+			die();
+		}
+		
+		static function dieAndDumpPretty($value) {
+			echo '<pre>';
+			print_r($value);
+			echo '</pre>';
+			die();
+		}
+		
+		static function dumpPrettyAndContinue($value) {
+			echo '<pre>';
+			print_r($value);
+			echo '</pre>';
+		}
+		
+		static function dumpAndContinue($value) {
+			echo '<pre>';
+			var_dump($value);
+			echo '</pre>';
+		}
+		
+		static function parseURI($value) {
+			return \parse_url($value);
+		}
+	}
