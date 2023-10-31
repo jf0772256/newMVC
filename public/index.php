4018b7cfaf5f7aff499a1182684725af41f04a16
Jesse Fender <jessefender@jessefender.com>
2023-10-31 17:26:04 -0500

    rename some values...

--- public/index.php
+++ public/index.php
@@ -5,18 +5,11 @@
 	
 	require_once __DIR__ . "/../vendor/autoload.php";
 	$config = require_once "../app/config/config.php";
-	// [
-	// 	"rootPath" => dirname(__DIR__, 1),
-	// 	"envPath" => dirname(__DIR__, 1) . "/.env"
-	// ];
 	
 	$app = new Application($config);
 	
-	//	$data = Application::$app->connection->ExecuteQuery("SELECT 'Hello World! Using Static' as text_col", [])->fetchObject()->text_col;
-	//	echo "does this show: {$data}";
-	
-	$externalRouter = require_once '../routes/base.routes.php';
-	$app->router->use($externalRouter);
+	$baselRouter = require_once '../routes/base.routes.php';
+	$app->router->use($baseRouter);
 	
 	// internal routes -- Note though that any routes added with teh same route and method will overwrite preceding routes
 	$app->router->get('/users', function()
@@ -27,6 +20,12 @@
 	
 	// testing that the params works
 	$app->router->get('/user/{id}', function(Request $request)
+	{
+		echo "<h1>Hello Welcome to the users page</h1>";
+		echo "<p>you requested user {$request->params['id']}</p>";
+	});
+	// testing that the params works
+	$app->router->get('/user', function(Request $request)
 	{
 		echo "<h1>Hello Welcome to the users page</h1>";
 		echo "<p>you requested user {$request->params['id']}</p>";
