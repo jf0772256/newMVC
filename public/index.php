eb2b6a143efcf79b52910d60e281faa1286825e8
Jesse Fender <jessefender@jessefender.com>
2023-10-30 23:39:47 -0500

    Testing parameters

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 	
 	use Jesse\SimplifiedMVC\Application;
+	use Jesse\SimplifiedMVC\Request;
 	
 	require_once __DIR__ . "/../vendor/autoload.php";
 	$config = require_once "../app/config/config.php";
@@ -17,11 +18,19 @@
 	$externalRouter = require_once '../routes/base.routes.php';
 	$app->router->use($externalRouter);
 	
-	// other routes maybe???
+	// internal routes -- Note though that any routes added with teh same route and method will overwrite preceding routes
 	$app->router->get('/users', function()
 	{
 		echo "<h1>Hello Welcome to the users page</h1>";
 		echo "<p>No Users were found</p>";
 	});
+	
+	// testing that the params works
+	$app->router->get('/user/{id}', function(Request $request)
+	{
+		echo "<h1>Hello Welcome to the users page</h1>";
+		echo "<p>you requested user {$request->params['id']}</p>";
+	});
+	
 	
 	$app->run();
