5899da4a1b8824d020823f0cfc416477640aa52c
Jesse Fender <jessefender@jessefender.com>
2023-10-30 23:29:14 -0500

    fixed missing route in method array

--- app/IRouter.php
+++ app/IRouter.php
@@ -20,12 +20,13 @@
 			];
 		}
 		
-		protected function requestRoutesArray($caller, ?string $method = null, ?string $route = null) : array
+		protected function requestRoutesArray($caller, ?string $method = null, ?string $route = null) : array|callable|bool
 		{
 			if ($caller instanceof Router) throw new InvalidArgumentException("Caller type mismatch. must be caller type of Jesse\SimplifiedMVC\Router", 400);
 			if (empty($method) && empty($route)) return $this->routes;
 			if (!empty($method) && empty($route)) return $this->routes[$method];
-			if (!empty($method) && !empty($route)) return $this->routes[$method][$route];
+			if (!empty($method) && !empty($route) && array_key_exists($route, $this->routes[$method])) return $this->routes[$method][$route];
+			if (!empty($method) && !empty($route) && !array_key_exists($route, $this->routes[$method])) return false;
 			throw new InvalidArgumentException("Out of order parameters", 400);
 		}
 		
