d5e37594a3d9d517d0887f854477161b4596e8d3
Jesse Fender <jessefender@jessefender.com>
2023-10-30 23:30:48 -0500

    Added an external routes page that can be imported and used

---
+++ routes/base.routes.php
@@ -0,0 +1,22 @@
+<?php
+	
+	use Jesse\SimplifiedMVC\Request;
+	use Jesse\SimplifiedMVC\Response;
+	use Jesse\SimplifiedMVC\Router;
+	
+	require_once __DIR__ . "/../vendor/autoload.php";
+	
+	$router = new Router(new Request(), new Response());
+	
+	$router->get('/', function ()
+	{
+		echo "<h1>Hello World!</h1>";
+	});
+	$router->get("/about", function ()
+	{
+		echo "<h1>About Us!</h1>";
+		echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur cumque enim error, exercitationem facere maxime minus molestiae nam nobis non perferendis porro quidem temporibus unde! Earum in itaque repellendus!</p>";
+	});
+	
+	// return completed router
+	return $router;
