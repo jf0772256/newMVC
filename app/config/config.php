1cb1fcdaeb2e02b8f5e3fda469450b2f40084134
Jesse Fender <jessefender@jessefender.com>
2023-10-31 19:33:38 -0500

    added routes path

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,6 @@
 <?php
 	return [
 		"rootPath" => dirname(__DIR__, 2),
-		"envPath" => dirname(__DIR__, 2) . '/.env'
+		"envPath" => dirname(__DIR__, 2) . '/.env',
+		"routesPath" => dirname(__DIR__, 2) . '/routes'
 	];
