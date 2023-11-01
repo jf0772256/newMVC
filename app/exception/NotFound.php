b6391a740c01231e52fca7fc0c593f14b04013f1
Jesse Fender <jf0772256@users.noreply.github.com>
2023-10-31 22:42:20 -0500

    Delete app/exception directory
    need to reupload

--- app/exception/NotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	namespace Jesse\SimplifiedMVC\Exception;
-	
-	class NotFound extends \Exception
-	{
-		protected $code = 404;
-		protected $message = "The page you're looking for couldn't be found";
-	}
