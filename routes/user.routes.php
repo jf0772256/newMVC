1c1344cd6c9d6623e4e388bd3a6158ed9aab06a5
Jesse Fender <jessefender@jessefender.com>
2023-10-31 19:36:16 -0500

    added route for the on index router routes to their own file

---
+++ routes/user.routes.php
@@ -0,0 +1,33 @@
+<?php
+	
+	use Jesse\SimplifiedMVC\Request;
+	use Jesse\SimplifiedMVC\Response;
+	use Jesse\SimplifiedMVC\Router;
+	
+	require_once __DIR__ . "/../vendor/autoload.php";
+	
+	$router = new Router(new Request(), new Response());
+	
+	// internal routes -- Note though that any routes added with teh same route and method will overwrite preceding routes
+	$router->get('/users', function()
+	{
+		echo "<h1>Hello Welcome to the users page</h1>";
+		echo "<p>No Users were found</p>";
+	});
+	
+	// testing that the params works
+	$router->get('/user/{id}', function(Request $request)
+	{
+		echo "<h1>Hello Welcome to the users page</h1>";
+		echo "<p>you requested user {$request->params['id']}</p>";
+	});
+	
+	// testing that the params works
+	$router->get('/user', function(Request $request)
+	{
+		echo "<h1>Hello Welcome to the users page</h1>";
+		echo "<p>you requested user {$request->params['id']}</p>";
+	});
+	
+	// return completed router
+	return $router;
