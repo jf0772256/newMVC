94ad03113ce9306bc2d98bce6b9ca1180d8380f6
Jesse Fender <jessefender@jessefender.com>
2023-10-31 19:35:39 -0500

    added route for dumping server vars for testing

--- routes/site.routes.php
+++ routes/site.routes.php
@@ -3,6 +3,7 @@
 	use Jesse\SimplifiedMVC\Request;
 	use Jesse\SimplifiedMVC\Response;
 	use Jesse\SimplifiedMVC\Router;
+	use Jesse\SimplifiedMVC\Utilities\Utility;
 	
 	require_once __DIR__ . "/../vendor/autoload.php";
 	
@@ -12,10 +13,17 @@
 	{
 		echo "<h1>Hello World!</h1>";
 	});
+	
 	$router->get("/about", function ()
 	{
 		echo "<h1>About Us!</h1>";
 		echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur cumque enim error, exercitationem facere maxime minus molestiae nam nobis non perferendis porro quidem temporibus unde! Earum in itaque repellendus!</p>";
+	});
+	
+	$router->get('/serverTest', function ($request, $response)
+	{
+		header('Content-Type: application/json');
+		Utility::dieAndDumpPretty($_SERVER);
 	});
 	
 	// return completed router
