b46bb56c804cfe524ddb205a1bb0a6ee982ce6a6
Jesse Fender <jessefender@jessefender.com>
2023-10-31 22:47:29 -0500

    I think this commit when merged will be fixed...

--- app/Exception/NotFound.php
+++ app/Exception/NotFound.php
@@ -1,8 +1,8 @@
 <?php
 	
 	namespace Jesse\SimplifiedMVC\Exception;
-	
-	class NotFound extends \Exception
+	use Exception;
+	class NotFound extends Exception
 	{
 		protected $code = 404;
 		protected $message = "The page you're looking for couldn't be found";
