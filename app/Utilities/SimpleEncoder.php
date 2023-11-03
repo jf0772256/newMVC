72f457bba3d5a0dba94ec49a26bddf1b3a8d22c3
Jesse Fender <jessefender@jessefender.com>
2023-11-02 22:10:33 -0500

    Made some changes to the class to use teh token given to it ... rather than storing it in teh class. only the key is passed and it is done in the constructor... don't forget to set this!

--- app/Utilities/SimpleEncoder.php
+++ app/Utilities/SimpleEncoder.php
@@ -4,34 +4,33 @@
 	
 	class SimpleEncoder
 	{
-		private string $token;
 		private static string $key;
 		
-		public function __construct(string $token, ?string $key)
+		public function __construct(string $key)
 		{
-			if (!empty($key)) SimpleEncoder::$key = $key;
-			if (empty(SimpleEncoder::$key)) openssl_random_pseudo_bytes(512);
-			$this->token = $token;
+			SimpleEncoder::$key = $key;
 		}
 		
-		public function encode () : string
+		public function encode (string $token) : string
 		{
-			$this->token = str_rot13($this->token);
-			$this->token = gzdeflate($this->token,7);
-			$this->token = $this->xor($this->token);
-			$this->token = base64_encode($this->token);
+			
+			$token = str_rot13($token);
+			$token = gzdeflate($token,7);
+			$token = $this->xor($token);
+			$token = base64_encode($token);
 			// make url safe
-			$this->token = urlencode($this->token);
-			return $this->token;
+			$token = str_replace(['+','/','='],['_','.',''],$token);
+			return $token;
 		}
-		public function decode () : string
+		public function decode (string $token) : string
 		{
 			// make url safe
-			$this->token = urldecode($this->token);
-			$this->token = base64_decode($this->token);
-			$this->token = $this->xor($this->token);
-			$this->token = gzinflate($this->token);
-			$this->token = str_rot13($this->token);
+			$token = str_replace(['_','.'],['+','/'],$token);
+			$token = base64_decode($token);
+			$token = $this->xor($token);
+			$token = gzinflate($token);
+			$token = str_rot13($token);
+			return $token;
 		}
 		
 		private function xor(string $input) : string
