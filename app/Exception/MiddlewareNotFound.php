c678f8c4d69097597f62bd123956608a4cf9464c
Jesse Fender <jessefender@jessefender.com>
2023-11-06 20:51:48 -0600

    Fixed MiddlewareNotFound.php to wrap middleware key in a string so its cleat that that's what the middle ware was that was passed

--- app/Exception/MiddlewareNotFound.php
+++ app/Exception/MiddlewareNotFound.php
@@ -8,7 +8,7 @@
 		protected $message;
 		public function __construct (string $key)
 		{
-			$this->message = "The middleware that you attempted to load was not found ({$key}). Make sure that you have created the class and have added the class to the lookup MAP constant in Middleware.php";
+			$this->message = "The middleware that you attempted to load was not found ('{$key}'). Make sure that you have created the class and have added the class to the lookup MAP constant in Middleware.php";
 			parent::__construct($this->message, $this->code);
 		}
 	}
