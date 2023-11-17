d52b85343c3ef93f675f1112cc7bac2868b69a5c
Jesse Fender <jessefender@jessefender.com>
2023-11-16 18:25:44 -0600

    add docker site env path to config.php
    this has some additional details for running in docker containers

--- app/config/config.php
+++ app/config/config.php
@@ -2,5 +2,6 @@
 	return [
 		"rootPath" => dirname(__DIR__, 2),
 		"envPath" => dirname(__DIR__, 2) . '/.env',
+		"dockerSiteEnvPath" => dirname(__DIR__, 2) . '/dockerSite.env',
 		"routesPath" => dirname(__DIR__, 2) . '/routes'
 	];
