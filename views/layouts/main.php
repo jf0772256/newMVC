6d086c2b68283d6d5991d84f6e223ab91d29ebed
Jesse Fender <jessefender@jessefender.com>
2023-11-20 14:00:31 -0600

    added var for dynamic titling

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,7 @@
 <?php
 //
+	
+	$title = $title ?? 'MyMVC Home';
 ?>
 
 <!doctype html>
@@ -9,7 +11,7 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>MyMVC Home</title>
+	<title><?=$title?></title>
 	<link rel="stylesheet" href="./css/bootstrap.min.css">
 	<script src="./js/bootstrap.bundle.js"></script>
 	<link rel="stylesheet" href="./fontawesome/all.css">
