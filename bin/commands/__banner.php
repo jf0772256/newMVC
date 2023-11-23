fd5681bfd2ec6bcaba71fa8b6e739309682eb90f
Jesse Fender <jessefender@jessefender.com>
2023-11-22 22:09:05 -0600

    Because i can :)

---
+++ bin/commands/__banner.php
@@ -0,0 +1,16 @@
+<?php
+	$banner = PHP_EOL . PHP_EOL;
+	$banner .= "**************************************************" . PHP_EOL;
+	$banner .= "      &&&&&&&&&&&&&&&       &&&&&&&&&&&&&&&       " . PHP_EOL;
+	$banner .= "      &&&&&&&&&&&&&&&       &&&&&&&&&&&&&&&       " . PHP_EOL;
+	$banner .= "            &&&             &&&                   " . PHP_EOL;
+	$banner .= "            &&&             &&&                   " . PHP_EOL;
+	$banner .= "            &&&             &&&&&&&&&&            " . PHP_EOL;
+	$banner .= "            &&&             &&&&&&&&&&            " . PHP_EOL;
+	$banner .= "            &&&             &&&                   " . PHP_EOL;
+	$banner .= "      &&&   &&&             &&&                   " . PHP_EOL;
+	$banner .= "      &&&\_/&&&             &&&                   " . PHP_EOL;
+	$banner .= "      \&&&&&&&/             &&&                   " . PHP_EOL;
+	$banner .= "**************************************************" . PHP_EOL . PHP_EOL;
+	
+	return $banner;
---
+++ bin/commands/subCommand/mig_build.php
@@ -0,0 +1,24 @@
+<?php
+	
+	
+echo "Migration Build script starting..." . PHP_EOL;
+echo "Loading required scripts..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Checking Database Connection..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Creating Migration Table..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Creating Directory..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Setting Permissions..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Testing Permissions..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Removing Test..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Attempting to auto generate migrations based on models in http/Models/ folder..." . PHP_EOL;
+echo "OK..." . PHP_EOL;
+echo "Done..." . PHP_EOL . PHP_EOL;
+
+
+exit();
