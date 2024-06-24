4632d9bd20c5f2ca97835f91c35e7a053b040322
Jesse Fender <jessefender@jessefender.com>
2024-06-24 10:21:55 -0500

    The nifty way I am managing the console command registration, This gets modified when you add a command using the create:command, command. This was done this way to allow youd to create new commands as you see fit to do so

---
+++ app/bin/commands_list.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	\Jesse\SimplifiedMVC\bin\commands\ExampleCommand::class,
+	\Jesse\SimplifiedMVC\bin\commands\MigrateRun::class,
+	\Jesse\SimplifiedMVC\bin\commands\MigrateCreate::class,
+	\Jesse\SimplifiedMVC\bin\commands\CreateCommand::class,
+	\Jesse\SimplifiedMVC\bin\commands\CreateController::class,
+	\Jesse\SimplifiedMVC\bin\commands\CreateMiddleware::class,
+	\Jesse\SimplifiedMVC\bin\commands\CreateModel::class,
+	\Jesse\SimplifiedMVC\bin\commands\CreateView::class,
+	\Jesse\SimplifiedMVC\bin\commands\CreateViewLayout::class,
+	\Jesse\SimplifiedMVC\bin\commands\GenerateDocs::class,
+	\Jesse\SimplifiedMVC\bin\commands\SignCommand::class,
+	\Jesse\SimplifiedMVC\bin\commands\GenerateKeyCommand::class,
+];
+
+?>
