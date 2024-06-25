7985ee12f59ac86f3214a45008b9dfdf33dcf908
Jesse Fender <jessefender@jessefender.com>
2024-06-24 22:24:58 -0500

    This should have a terminating /

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
 		"rootPath" => dirname(__DIR__, 2),
 		"envPath" => dirname(__DIR__, 2) . '/.env',
 		"dockerSiteEnvPath" => dirname(__DIR__, 2) . '/dockerSite.env',
-		"routesPath" => dirname(__DIR__, 2) . '/routes'
+		"routesPath" => dirname(__DIR__, 2) . '/routes/'
 	];
