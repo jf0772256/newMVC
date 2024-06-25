32ae48f4d34dca4006802eeeac0f42aa5fdcd6f2
Jesse Fender <jessefender@jessefender.com>
2024-06-24 21:39:46 -0500

    moved create method from the user controller to the API controller

--- http/Controllers/userController.php
+++ http/Controllers/userController.php
@@ -28,40 +28,6 @@
 		}
 		
 		/**
-		 * @throws BadRequest
-		 */
-		function create (Request $request, Response $response) : string
-		{
-			// create a new user here...
-			$userId = 0;
-			try
-			{
-				$request->requestExpects('application/json');
-				$body = $request->getRequestBody();
-				$user = new User();
-				$user->loadData($body);
-				$user->validate();
-				if (count($user->errors) > 0)
-				{
-					$response->customErrorCode("Request body contains errors. Please see the errors object for details.", 400);
-					header("Content-Type: application/json; charset: utf-8");
-					return json_encode($user);
-				}
-				
-			}
-			catch (\Throwable $e)
-			{
-				$exception = new BadRequest("REQUEST.BODY.INVALID", 400, $e);
-				$respData = ["message" => "REQUEST.BODY.INVALID", "code" => 400, "previousError" => ["message" => $e->getMessage() ?? "NO.PREVIOUS.ERROR", "code" => $e->getCode() ?? "NO.PREVIOUS.ERROR"]];
-				$response->statusCode($exception->getCode());
-				header("Content-Type: application/json; charset: utf-8");
-				die(json_encode($respData));
-			}
-			$response->customErrorCode("Data Accepted & Processed Successfully, OK.", 200);
-			return json_encode(['result' => true, 'userId' => $userId, 'message' => 'User was created successfully']);
-		}
-		
-		/**
 		 * @throws \Exception
 		 */
 		function register (Request $request) : string
@@ -95,8 +61,9 @@
 				if ($login->validate() && $login->login())
 				{
 					// set session and redirect
-					Application::$app->session->setValue('authenticated', true);
 					Application::$app->user = User::find(['email' => $login->email]);
+					Application::$app->session->setValue('authenticated', true);
+					Application::$app->session->setValue('user', Application::$app->user->id);
 					Application::$app->response->redirect('/');
 				}
 			}
@@ -106,6 +73,7 @@
 		function logout(Request $request) : string
 		{
 			Application::$app->session->clear('authenticated');
+			Application::$app->session->clear('user');
 			Application::$app->user = new User();
 			Application::$app->response->redirect('/');
 		}
