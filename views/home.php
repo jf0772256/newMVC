d1c709a5a340207deebb9caf490e904093dd39b5
Jesse Fender <jessefender@jessefender.com>
2024-06-24 19:05:21 -0500

    added a log out form to test the logout, since sessions were holding open just shortly, this will trigger the page to bounce unauthenticated users out to the login page

--- views/home.php
+++ views/home.php
@@ -1,3 +1,13 @@
 <?php ?>
 
 <h1>Hello World</h1>
+<?php
+	
+	use Jesse\SimplifiedMVC\Form\Form;
+	use Jesse\SimplifiedMVC\Http\Models\User;
+	
+	$user = new User();
+	$form = Form::begin('/logout', 'post', '');
+	$form->field($user, '')->generateSubmit('','Log Out')->renderField();
+	Form::end();
+?>
