6a9906016ad873045b2da1d66f360506859e359b
Jesse Fender <jessefender@jessefender.com>
2024-07-01 15:19:31 -0500

    updating the user.routes.php file to demonstrate the changes to the Router class

--- routes/user.routes.php
+++ routes/user.routes.php
@@ -8,22 +8,30 @@
 	require_once __DIR__ . "/../vendor/autoload.php";
 	
 	$router = new Router(new Request(), new Response());
+	$router->controller(userController::class)->prefix('/user');
 	
 	// internal routes -- Note though that any routes added with the same route and method will overwrite preceding routes
 	// see below... the last route overwrites this one
-	$router->get('/user', function(Request $request, Response $response) { $response->redirect('/users'); });
-	$router->get('/user/{id}', [userController::class, 'authUserView'])->only('auth');
 	
-	$router->get('/users', [userController::class, 'guestUserView'])->only('guest');
-	$router->get('/users/{id}', [userController::class, 'guestUserView'])->only('guest');
+	$router->get('/', function(Request $request, Response $response) { $response->redirect('/users'); });
+	$router->get('/{id}', 'authUserView')->only('auth');
 	
-	$router->get('/register', [userController::class, 'register'])->only('guest');
-	$router->post('/register', [userController::class, 'register'])->only('guest');
+	//change the prefix to a different one
+	$router->prefix('/users');
 	
-	$router->get('/login', [userController::class, 'login'])->only('guest');
-	$router->post('/login', [userController::class, 'login'])->only('guest');
+	$router->get('/', 'guestUserView')->only('guest');
+	$router->get('/{id}', 'guestUserView')->only('guest');
 	
-	$router->post('/logout', [userController::class, 'logout'])->only('auth');
+	// clearing the prefix
+	$router->prefix();
+	
+	$router->get('/register', 'register')->only('guest');
+	$router->post('/register', 'register')->only('guest');
+	
+	$router->get('/login', 'login')->only('guest');
+	$router->post('/login', 'login')->only('guest');
+	
+	$router->post('/logout', 'logout')->only('auth');
 	
 	// return completed router
 	return $router;
