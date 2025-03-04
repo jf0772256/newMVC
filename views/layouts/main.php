23418184a36fdb44d459ed56f0f3bfbc05d143e7
Jesse Fender <jessefender@jessefender.com>
2025-03-03 19:58:54 -0600

    Adding some basic fancifying the base layout - adding bootstrap CDN links. These links can be changed at the users will.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -12,8 +12,8 @@
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?=$title?></title>
-	<link rel="stylesheet" href="/css/bootstrap.min.css">
-	<script src="/js/bootstrap.bundle.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 	<link rel="stylesheet" href="/fontawesome/css/all.css">
 </head>
 <body>
