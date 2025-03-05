e2866710ed276e6fda9d32cfc8fc36d90fab4275
Jesse Fender <jessefender@jessefender.com>
2025-03-04 20:48:51 -0600

    Testing something with the DataModel class now using the queryBuilder... It's so much more understandable with simple fluid methodology. This now does the same as before the commit but 22 lines of code fewer and less verbose in the method calls the work is done in the query builder methods.

--- app/DataModel.php
+++ app/DataModel.php
@@ -10,37 +10,29 @@
 		abstract static public function attributes() : array;
 		abstract static public function primaryKey() : string;
 		function labels () : array { return []; }
-		function save() {
+		function save(): void
+		{
 			$tableName = static::tableName();
 			$attributes = static::attributes();
 			$params = [];
-			foreach($attributes as $attribute) $params[] = $this->{$attribute};
-			$stmnt = $this->prepare("INSERT INTO {$tableName} (" . implode(',', $attributes) . ") VALUES (" . implode(',', array_fill(0, count($attributes), '?')) . ")", $params);
-			$rows = -1;
-			$rows = $stmnt->rowCount();
-			$stmnt->closeCursor();
-			return $rows;
+			foreach($attributes as $attribute) $params[$attribute] = $this->{$attribute};
+			Application::$app->builder->build(Application::$app->builder->builder()->insert($tableName, $params));
 		}
-		function prepare($sql, $params) { return Application::$app->connection->ExecuteQuery($sql, $params); }
 		static function findOne (array $where) : PDOStatement
 		{
 			// do query, find user and return user object to caller
 			$table = static::tableName();
-			$sql = "SELECT * FROM {$table} WHERE ";
-			$values = [];
-			foreach (array_keys($where) as $index => $searchString)
-			{
-				$sql .=  ($index == 0)  ? "{$searchString} = ?" : " AND {$searchString} = ?";
-				$values[] = $where[$searchString];
-			}
-			return Application::$app->connection->ExecuteQuery($sql, $values);
+			$builder = Application::$app->builder->builder();
+			$builder->select($table, ['*']);
+			foreach(array_keys($where) as $index => $value) $index === 0 ? $builder->where($value, '=', $where[$value]) : $builder->andWhere($value, '=', $where[$value]);
+			return Application::$app->builder->build($builder);
 		}
 		static function fetchAll(?array $orderBy = []) : PDOStatement
 		{
 			$table = static::tableName();
 			$sql = "SELECT * FROM {$table}";
 			foreach ($orderBy as $key => $value) $sql .= $key === 0 ? " ORDER BY {$value}" : ", {$value}";
-			return Application::$app->connection->ExecuteQuery($sql);
+			return Application::$app->builder->build(Application::$app->builder->builder()->raw($sql));
 		}
 		function update() : void
 		{
@@ -48,41 +40,27 @@
 			$attributes = static::attributes();
 			$pk = static::primaryKey();
 			$params = [];
-			$fieldString = "";
-			foreach($attributes as $attribute)
-			{
-				$fieldString .= " {$attribute}=?,";
-				$params[] = $this->{$attribute};
-			}
-			$fieldString = rtrim(',', $fieldString);
-			$params[] = $this->{$pk};
-			$sql = "UPDATE {$tableName} SET{$fieldString} WHERE {$pk}=?";
-			Application::$app->connection->ExecuteQuery($sql, $params);
+			foreach($attributes as $attribute) $params[$attribute] = $this->{$attribute};
+			Application::$app->builder->build(Application::$app->builder->builder()
+				->update($tableName, $params)
+				->where($pk, '=', $this->{$pk})
+			);
 		}
 		function patch(array $field2Values) : void
 		{
 			$tableName = static::tableName();
 			$pk = static::primaryKey();
 			$params = [];
-			$fieldString = "";
-			foreach($field2Values as $attribute)
-			{
-				if ($attribute === "id") continue;
-				$fieldString .= " {$attribute}=?,";
-				$params[] = $this->{$attribute};
-			}
-			$params[] = $this->{$pk};
-			$sql = "UPDATE {$tableName} SET{$fieldString} WHERE {$pk}=?";
-			Application::$app->connection->ExecuteQuery($sql, $params);
-			
+			foreach($field2Values as $attribute) if ($attribute !== $pk) $params[$attribute] = $this->{$attribute};
+			Application::$app->builder->build(Application::$app->builder->builder()
+				->update($tableName, $params)
+				->where($pk, '=', $this->{$pk})
+			);
 		}
 		function delete() : void
 		{
 			$tableName = static::tableName();
 			$pk = static::primaryKey();
-			$params = [];
-			$params[] = $this->{$pk};
-			$sql = "DELETE FROM {$tableName} WHERE {$pk} = ?";
-			Application::$app->connection->ExecuteQuery($sql, $params);
+			Application::$app->builder->build(Application::$app->builder()->delete($tableName)->where($pk, '=', $this->{$pk}));
 		}
 	}
